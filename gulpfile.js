f4bca7b205d86fafca68fc51665f1800d874ed1d
Roux Volonte <rxvolonte@gmail.com>
2016-03-14 22:04:07 -0300

    Iniciando configurações do gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// Plugins
+var gulp = require('gulp');
+
+// Tarefas
+gulp.task('info', function() {
+  console.log("===================================================");
+  console.log("Wellcome to Boilerplate.Dev | powered by @rxvolonte");
+  console.log("Date: 2016-03-14 | Hour: 21:28");
+  console.log("Url: https://github.com/RxVolonte/boilerplate.dev");
+  console.log("Version: 1.0.0 | Last Update: 2016-03-14");
+  console.log("===================================================");
+});
+
+gulp.task('help', function() {
+  console.log("===================================================");
+  console.log("HELP!");
+  console.log("===================================================");
+  console.log("TASK | DESCRIPTION");
+  console.log("===================================================");
+  console.log("info | Show info about this project");
+  console.log("===================================================");
+});
